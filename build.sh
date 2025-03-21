e70b2051568c8c5d0152821b22775f973705826a
jacopouggeri <jacopo.uggeri@gmail.com>
2025-03-21 18:57:52 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Generating mod archive from: $(pwd)"
+# Extract data from modinfo.json using jq
+VERSION=$(jq -r '.version' modinfo.json)
+MODID=$(jq -r '.modid' modinfo.json)
+# Clear any existing archive
+rm -f "${MODID}"_*.zip
+# Define mod version and output file name
+OUTPUT="${MODID}_${VERSION}.zip"
+zip -r "${OUTPUT}" ./ -x ".*" "*.zip" "*.DS_STORE" "changelog.txt" "build.sh"
+echo "Mod archive created: ${OUTPUT}"
