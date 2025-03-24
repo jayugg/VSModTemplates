8c9a1e98f28b49d7493344097c1c51a0130a81e1
jacopouggeri <jacopo.uggeri@gmail.com>
2025-03-24 22:41:29 +0000

    v1.3.0 changelog.txt for changes

--- templates/VSContentModTemplate/scripts/client.sh
+++ templates/VSContentModTemplate/scripts/client.sh
@@ -1,8 +1,11 @@
 # Unused by VS Code tasks
-"build.sh"
+chmod +x "$(pwd)/scripts/build.sh"
+"$(pwd)/scripts/build.sh"
 echo "Launching game client..."
-//#if (ArmMacOsLaunchSettings)
-"$VINTAGE_STORY/Vintagestory" --tracelog --addModPath "{$(pwd)}"/Releases"
+//#if (SystemSpecificLaunchSettings == SuppressWindowsConsoleWindow)
+"$VINTAGE_STORY/Vintagestory.exe" --tracelog --addModPath "$(pwd)/Releases"
+//#elif (SystemSpecificLaunchSettings == ArmMacOsLaunchSettings)
+"$VINTAGE_STORY/Vintagestory" --tracelog --addModPath "$(pwd)/Releases"
 #else
-dotnet "$VINTAGE_STORY/Vintagestory.dll" --tracelog --addModPath "{$(pwd)}"/Releases"
+dotnet "$VINTAGE_STORY/Vintagestory.dll" --tracelog --addModPath "$(pwd)/Releases"
 //#endif
--- templates/VSContentModTemplate/scripts/server.sh
+++ templates/VSContentModTemplate/scripts/server.sh
@@ -1,8 +1,11 @@
 # Unused by VS Code tasks
-"build.sh"
+chmod +x "$(pwd)/scripts/build.sh"
+"$(pwd)/scripts/build.sh"
 echo "Launching game client..."
-//#if (ArmMacOsLaunchSettings)
-"$VINTAGE_STORY/VintagestoryServer" --tracelog --addModPath "{$(pwd)}"/Releases"
+//#if (SystemSpecificLaunchSettings == SuppressWindowsConsoleWindow)
+"$VINTAGE_STORY/VintagestoryServer.exe" --tracelog --addModPath "$(pwd)/Releases"
+//#elif (SystemSpecificLaunchSettings == ArmMacOsLaunchSettings)
+"$VINTAGE_STORY/VintagestoryServer" --tracelog --addModPath "$(pwd)/Releases"
 #else
-dotnet "$VINTAGE_STORY/VintagestoryServer.dll" --tracelog --addModPath "{$(pwd)}"/Releases"
+dotnet "$VINTAGE_STORY/VintagestoryServer.dll" --tracelog --addModPath "$(pwd)/Releases"
 //#endif
